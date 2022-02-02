Entropy visualization fixes, speed up training by using mask.BACKWARDS_EQUAL instead of mask.BACKWARDS in the diagonal masking case in the training step.

cgt-mnist/datasets.py
@@ -16,8 +16,8 @@ def normalize_image(image, label):
     return tf.cast(image, dtype=tf.float32) / 255.0, label
 
 def find_centroids(config, ds_train):
-    num_clusters = config.n_colors
-    n_color_dims = config.n_color_dims
+    num_clusters = config.dataset.n_colors
+    n_color_dims = config.dataset.n_color_dims
     batch_size = config.kmeans_batch_size
     kmeans = MiniBatchKMeans(n_clusters=num_clusters, random_state=0, batch_size=batch_size, verbose=True)
     ds_batched = ds_train.map(normalize_image).batch(batch_size)
@@ -25,7 +25,7 @@ def find_centroids(config, ds_train):
         title = manager.status_bar(f"K-Means clustering to make {num_clusters}-color MNIST Dataset", justify=enlighten.Justify.CENTER)
         clusters_names = manager.status_bar(''.join('{:<10}'.format(f"cen. {i}") for i in range(num_clusters)))
         clusters_status = manager.status_bar(''.join('{:<10}'.format('??????') for _ in range(num_clusters)))
-        pbar = manager.counter(total=60000//kmeans_batch_size, desc='Discretize to 8 colors', unit='minibatches')
+        pbar = manager.counter(total=60000//batch_size, desc='Discretize to 8 colors', unit='minibatches')
         for img, _ in pbar(iter(ds_batched)):
             pixels = img.numpy().reshape(-1, 1)
             kmeans.partial_fit(pixels)
@@ -104,20 +104,47 @@ class Datasets:
         self.ds_test_orig = ds_test_orig
     
     def quantize(self, sequence, label):
-        d = squared_euclidean_distance(sequence, self.centroids) # (height * width, n_centroids)
+        d = squared_distance(sequence, self.centroids) # (height * width, n_centroids)
         sequence = tf.math.argmin(d, axis=1, output_type=tf.int32)  # (height * width)
         return sequence, label
 
-    def unquantize(self, x):
+    def unquantize(self, x, to="same"):
         x_one_hot = tf.cast(tf.one_hot(x, depth=len(self.centroids)), dtype=tf.float32)  # (seq, num_centroids)
-        y = tf.linalg.matmul(x_one_hot,self.centroids)  # (seq, num_features)
-        print("unquantize output shape", y.shape)
+        if to == "rgb":
+            if self.centroids.shape[-1] == 1:
+                centroids = tf.tile(self.centroids, [1, 3])
+            elif self.centroids.shape[-1] == 3:
+                centroids = self.centroids
+            else:
+                assert False, "color_dims is not 1 or 3, can't use to='rgb'"
+        else:
+            centroids = self.centroids
+        y = tf.linalg.matmul(x_one_hot,centroids)  # (seq, num_features, n_color_dims)
         return y
+    
+    def to_grayscale_rgb(self, seq):
+        assert seq.shape[-1] == 1, "to_grayscale() only works if the final dim is length 1"
+        
+        # turn the final dim from [1] to [3] by filling the color value to r, g, and b
+        seq = tf.tile(seq, [1, 1, 3])
+        return seq
 
     def expected_col(self, probs):
-        centroids = tf.reshape(self.centroids, [1, 1, -1])
-        expected_color = tf.tensordot(probs, centroids, axes=([2], [2]))
-        print("expected_col output shape", expected_color.shape)
+        # shape: (batch, seq, n_colors)
+        
+        probs = tf.expand_dims(probs, axis=-1)
+        # shape: (batch, seq, n_colors, n_color_dim=1)
+        
+        # add batch and sequence dim to centroids
+        centroids = tf.expand_dims(tf.expand_dims(self.centroids, axis=0), axis=0)
+        # shape: (batch=1, seq=1, n_colors, n_color_dim)
+        
+        # this does linear interpolation between colors
+        expected_color = probs * centroids
+        # shape: (batch=1, seq=1, n_colors, n_color_dim)
+        expected_color = tf.reduce_sum(expected_color, axis=[-2])
+        # shape: (batch=1, seq=1, n_color_dim)
+        
         return expected_color
     
     def shuffle_and_add_indices(self, sequence, label):

cgt-mnist/training.py
@@ -16,6 +16,34 @@ import wandb
 import schedules
 import models
 
+def log2(x):
+    numerator = tf.math.log(x)
+    denominator = tf.math.log(tf.constant(2, dtype=numerator.dtype))
+    return numerator / denominator
+
+def entropy_of_probabilities(probabilities, do_print=False):
+    if do_print:
+        print("p(x) =", probabilities)
+    log_probabilities = log2(probabilities)
+    if do_print:
+        print("log p(x) =", log_probabilities)
+    entropies = -1 * tf.reduce_sum(probabilities*log_probabilities, axis=-1)
+    entropies = tf.expand_dims(entropies, axis=-1)
+    
+    entropies = entropies / log2(tf.cast(probabilities.shape[-1], tf.float32))
+    
+    if do_print:
+        print("H(X) =", entropies)
+    return entropies
+
+def entropy_of_logits(logits, do_print=False):
+    if do_print:
+        print("logits =", logits)
+    
+    probabilities = tf.nn.softmax(logits, axis=-1)
+    
+    return entropy_of_probabilities(probabilities, do_print)
+
 def wandb_init(config, model_name, resume):
     if config['use_wandb']:
         wandb.init(project='dist-mnist', entity='maxeonyx', name=model_name, config=config, resume=resume)
@@ -71,10 +99,10 @@ class Evaluator():
             accum_gradients = [tf.zeros_like(w, dtype=tf.float32) for w in weights]
             accum_loss = tf.constant(0, tf.float32)
 
-            if config.training_mode == 'query_next' or config.training_mode == 'combination':
+            if config.training_mode == 'query_next' or config.training_mode == 'full_combination':
                 float_steps += 1.0
                 enc_mask_type = models.MASK_BACKWARD_EQUAL
-                dec_mask_type = models.MASK_BACKWARD
+                dec_mask_type = models.MASK_BACKWARD_EQUAL
                 x_inp = colors_inp[:, :]
                 x_tar = colors_tar[:, :]
                 i_inp = idxs[:, :]
@@ -84,7 +112,7 @@ class Evaluator():
                 accum_gradients = [accum_grad+grad for accum_grad, grad in zip(accum_gradients, gradients)]
                 accum_loss += tf.reduce_mean(loss)
 
-            if config.training_mode == 'query_all' or config.training_mode == 'combination':
+            if config.training_mode == 'query_unknown':
                 float_steps += 1.0
                 enc_mask_type = models.MASK_NONE
                 dec_mask_type = models.MASK_NONE
@@ -92,6 +120,18 @@ class Evaluator():
                 x_tar = colors_tar[:, n:]
                 i_inp = idxs[:, :n]
                 i_tar = idxs[:, n:]
+                loss, gradients = train_step_inner_inner(x_inp, x_tar, i_inp, i_tar, enc_mask_type, dec_mask_type)
+                accum_gradients = [accum_grad+grad for accum_grad, grad in zip(accum_gradients, gradients)]
+                accum_loss += tf.reduce_mean(loss)
+
+            if config.training_mode == 'query_all' or config.training_mode == 'full_combination':
+                float_steps += 1.0
+                enc_mask_type = models.MASK_NONE
+                dec_mask_type = models.MASK_NONE
+                x_inp = colors_inp[:, :n]
+                x_tar = colors_tar[:, :]
+                i_inp = idxs[:, :n]
+                i_tar = idxs[:, :]
                 loss, gradients = train_step_inner_inner(x_inp, x_tar, i_inp, i_tar, enc_mask_type, dec_mask_type)
                 accum_gradients = [accum_grad+grad for accum_grad, grad in zip(accum_gradients, gradients)]
                 accum_loss += tf.reduce_mean(loss)
@@ -252,8 +292,7 @@ class Evaluator():
         probabilities = tf.nn.softmax(logits, axis=2)
 
         expected_col = self.ds.expected_col(probabilities)
-        expected_col = tf.concat([self.ds.unquantize(inp_colors), expected_col], axis=1)
-        expected_col = tf.squeeze(expected_col)
+        expected_col = tf.concat([self.ds.unquantize(inp_colors, to="grayscale"), expected_col], axis=1)
         
         evaluate_counter.update()
         
@@ -273,7 +312,7 @@ class Evaluator():
         evaluate_counter.close()
         return autoregressive_samples, expected_col
     
-    def evaluate_varying(self, all_colors, all_idxs, n_fn, entropy=False, manager=None):
+    def evaluate_varying(self, all_colors, all_idxs, n_fn, manager=None):
         """Autoregressively sample a bunch of images, each starting with n
            (produced by n_fn) real pixels"""
         batch_size = all_colors.shape[0]
@@ -292,30 +331,53 @@ class Evaluator():
             inp_colors = all_colors[:1, :n]
             
             tar_idxs = all_idxs[:1, n:]
-            
-            unq_inp = self.viz.unquantize(inp_colors)
-            bg_input = self.viz.scatter_on_bg(unq_inp, inp_idxs, output_length=seq_length)
+
+            logits = self.eval_step(inp_colors, inp_idxs, tar_idxs)
+            unq_inp = self.ds.unquantize(inp_colors)
+            unq_inp_rgb = self.ds.to_grayscale_rgb(unq_inp)
+            bg_input = self.viz.scatter_on_bg(unq_inp_rgb, inp_idxs, output_length=seq_length)
             if bg_all is None:
                 bg_all = bg_input
             else:
                 bg_all = tf.concat([bg_all, bg_input], axis=0)
 
-            logits = self.eval_step(inp_colors, inp_idxs, tar_idxs)
-            
-            if entropy:
-                expected_col = entropy_of_logits(logits)
-            else:
-                # softmax along color dimension
-                probabilities = tf.nn.softmax(logits, axis=2)
-                expected_col = self.ds.expected_col(probabilities)
+            # softmax along color dimension
+            probabilities = tf.nn.softmax(logits, axis=-1)
+            expected_col = self.ds.expected_col(probabilities)
                 
-            expected_col = tf.concat([self.ds.unquantize(inp_colors), expected_col], axis=1)
-            expected_col = tf.squeeze(expected_col)
-            expected_col = tf.expand_dims(expected_col, 0)
+            expected_col = tf.concat([unq_inp, expected_col], axis=-2)
+            
             all_expected_col = expected_col if all_expected_col is None else tf.concat([all_expected_col, expected_col], axis=0)
 
         evaluate_counter.close()
         return all_expected_col, bg_all
+    
+    def evaluate_varying_entropy(self, all_colors, all_idxs, n_fn, manager=None):
+        """Sample a bunch of images, each starting with n
+           (produced by n_fn) real pixels. Show the entropy over all outputs, including the inputs."""
+        batch_size = all_colors.shape[0]
+        seq_length = all_colors.shape[1]
+        
+        if manager is None:
+            manager = enlighten.get_manager()
+        evaluate_counter = manager.counter(total=batch_size, desc="Evaluating", unit='steps', leave=False)
+        
+        all_entropies = None
+        for i in evaluate_counter(range(batch_size)):
+            n = min(max(1, int(n_fn(i))), all_colors.shape[1] - 1)
+            inp_idxs = all_idxs[:1, :n]
+            inp_colors = all_colors[:1, :n]
+            
+            tar_idxs = all_idxs[:1, :]
+
+            logits = self.eval_step(inp_colors, inp_idxs, tar_idxs)
+            
+            entropies = entropy_of_logits(logits)
+                
+            all_entropies = entropies if all_entropies is None else tf.concat([all_entropies, entropies], axis=0)
+
+        evaluate_counter.close()
+        return all_entropies
 
     def process_batch(self, return_figs=False, show_input=True, show_output=True, manager=None):
         all_colors = self.test_colors
@@ -326,29 +388,32 @@ class Evaluator():
         half_colors = all_colors[:, :n]
         half_idxs = all_idxs[:, :n]
         image_height, image_width = self.config.dataset.image_size
+        figs = []
         if show_input:
             self.viz.showSeq(half_colors, half_idxs, (image_width, image_height), batch_size, unshuffle=True)
             self.viz.showSeq(all_colors, all_idxs, (image_width, image_height), batch_size, unshuffle=True)
         if show_output:
             autoregressive_samples, expected_col = self.evaluate(half_colors, all_idxs, manager=manager)
-            fig1 = self.viz.showSeq(autoregressive_samples, all_idxs, (image_width, image_height), batch_size, unshuffle=True, return_fig=True)
-            fig2 = self.viz.showSeq(expected_col,           all_idxs, (image_width, image_height), batch_size, unshuffle=True, do_unquantize=False, return_fig=True)
+            figs.append(self.viz.showSeq(autoregressive_samples, all_idxs, (image_width, image_height), batch_size, unshuffle=True, return_fig=True))
+            figs.append(self.viz.showSeq(expected_col,           all_idxs, (image_width, image_height), batch_size, unshuffle=True, do_unquantize=False, return_fig=True))
             repeated_col = tf.tile(all_colors[5:6], [batch_size, 1])
             repeated_idxs = tf.tile(all_idxs[5:6], [batch_size, 1])
             varying_n, varying_in = self.evaluate_varying(repeated_col, repeated_idxs, n_fn=lambda i: self.config.dataset.seq_length//(batch_size+1)*(i+1), manager=manager)
-            e_varying_n, e_varying_in = self.evaluate_varying(repeated_col, repeated_idxs, n_fn=lambda i: self.config.dataset.seq_length//(batch_size+1)*(i+1), entropy=True, manager=manager)
+            e_varying_n = self.evaluate_varying_entropy(repeated_col, repeated_idxs, n_fn=lambda i: self.config.dataset.seq_length//(batch_size+1)*(i+1), manager=manager)
             if show_input:
                 self.viz.showSeq(varying_in,                repeated_idxs, (image_width, image_height), batch_size, unshuffle=False, do_unquantize=False)
-            fig3 = self.viz.showSeq(varying_n,              repeated_idxs, (image_width, image_height), batch_size, unshuffle=True, do_unquantize=False, return_fig=True)
-            fig3 = self.viz.showSeq(e_varying_n,              repeated_idxs, (image_width, image_height), batch_size, unshuffle=True, do_unquantize=False, return_fig=True)
+            figs.append(self.viz.showSeq(varying_n,              repeated_idxs, (image_width, image_height), batch_size, unshuffle=True, do_unquantize=False, return_fig=True))
+            figs.append(self.viz.showSeq(e_varying_n,              repeated_idxs, (image_width, image_height), batch_size, unshuffle=True, do_unquantize=False, return_fig=True))
             repeated_col = tf.tile(all_colors[0:1], [batch_size, 1])
             repeated_idxs = tf.tile(all_idxs[0:1], [batch_size, 1])
             varying_n, varying_in = self.evaluate_varying(repeated_col, repeated_idxs, n_fn=lambda i: self.config.dataset.seq_length//(batch_size+1)*(i+1), manager=manager)
+            e_varying_n = self.evaluate_varying_entropy(repeated_col, repeated_idxs, n_fn=lambda i: self.config.dataset.seq_length//(batch_size+1)*(i+1), manager=manager)
             if show_input:
                 self.viz.showSeq(varying_in,                repeated_idxs, (image_width, image_height), batch_size, unshuffle=False, do_unquantize=False)
-            fig4 = self.viz.showSeq(varying_n,              repeated_idxs, (image_width, image_height), batch_size, unshuffle=True, do_unquantize=False, return_fig=True)
+            figs.append(self.viz.showSeq(varying_n,              repeated_idxs, (image_width, image_height), batch_size, unshuffle=True, do_unquantize=False, return_fig=True))
+            figs.append(self.viz.showSeq(e_varying_n,              repeated_idxs, (image_width, image_height), batch_size, unshuffle=True, do_unquantize=False, return_fig=True))
             if return_figs:
-                return fig1, fig2, fig3, fig4
+                return figs
     
     def many_autoregressive(self, n_seq=None):
         all_colors = self.test_colors
@@ -367,18 +432,14 @@ class Evaluator():
 
 class TrainingLoop():
     
-    def __init__(self, config, viz, model, optimizer, model_name):
+    def __init__(self, config, evaler, model_name):
         
         self.config = config
-        self.viz = viz
-        self.ds = ds
-        self.optimizer = optimizer
+        self.evaler = evaler
         
         self.model_name = model_name
         
-        self.ds_test = iter(ds_test)
-        
-        self.new_test_batch()
+        wandb_init(config, model_name, resume=False)
         
         self.step_index = 0
         self.last_eval_loss = None
@@ -389,8 +450,6 @@ class TrainingLoop():
         self.running_mean = 0.
         self.test_loss_shuf = 0.
         self.test_loss_seq = 0.
-        
-        self.train_step_grad_accum, self.train_step_normal, self.train_step_distributed, self.train_step_distributed_accum, self.eval_step = training_functions(config, model, optimizer, ds_train)
     
     def update_infobar(self, info_bar, loss):
         window_size = self.config['loss_window_size']
@@ -403,10 +462,9 @@ class TrainingLoop():
         
         info_bar.update(f'Loss: {loss:.5g}, Loss ({window_size} step avg.): {running_mean:.5g}, Test Loss (shuf): {test_loss_shuf:.5g}, Test Loss (seq): {test_loss_seq:.5g}, #R*BS*GA: {num_replicas:>3}*{minibatch_size}*{self.accum_steps:<5}')
         
-
         
-
     def train(self):
+        
         with enlighten.get_manager() as manager:
             status_bar = manager.status_bar(f"Training model '{self.model_name}'", justify=enlighten.Justify.CENTER)
             info_bar = manager.status_bar('Loss: ??????, Learning Rate: ???????, Batch Size: ???*?????')
@@ -427,7 +485,7 @@ class TrainingLoop():
         loss=0.
         
         if self.config.grad_accum_steps is None or self.config.grad_accum_steps == 1:
-            loss = self.train_step_distributed()
+            loss = self.evaler.train_step_distributed()
             self.accum_steps = 1
         else:
             if type(self.config.grad_accum_steps) is int:
@@ -443,7 +501,7 @@ class TrainingLoop():
                 bs_sched = schedules.batch_size_schedule(self.config, schedule_name, params)
                 self.accum_steps = bs_sched(self.step_index)
             self.update_infobar(info_bar, loss)
-            loss = self.train_step_distributed_accum(self.accum_steps)
+            loss = self.evaler.train_step_distributed_accum(self.accum_steps)
         
         self.loss_history[self.step_index] = loss
         if self.step_index > 0:
@@ -468,16 +526,25 @@ class TrainingLoop():
             self.last_eval_loss = self.running_mean
             self.last_eval_step = self.step_index
             print(f"Step {self.step_index}, Loss (last minibatch): {loss}, Loss ({window_size} step avg.): {self.last_eval_loss}")
-            fig1, fig2, fig3, fig4 = self.process_batch(return_figs=True, show_input=(self.step_index == 0), manager=manager)
+            fig1, fig2, fig3, fig4, fig5, fig6 = self.evaler.process_batch(return_figs=True, show_input=(self.step_index == 0), manager=manager)
             if self.config['use_wandb']:
                 self.last_log_index = self.step_index
-                wandb.log({'log_loss': tf.math.log(loss), 'learning_rate': self.optimizer._decayed_lr(tf.float32), 'viz_autoregressive': wandb.Image(fig1), 'viz_expected': wandb.Image(fig2), 'viz_varying': wandb.Image(fig3), 'viz_varying_sequential': wandb.Image(fig4)}, step=self.step_index)
+                wandb.log({
+                    'log_loss': tf.math.log(loss),
+                    'learning_rate': self.evaler.optimizer._decayed_lr(tf.float32),
+                    'viz_autoregressive': wandb.Image(fig1),
+                    'viz_expected': wandb.Image(fig2),
+                    'viz_varying': wandb.Image(fig3),
+                    'viz_varying_entropy': wandb.Image(fig4),
+                    'viz_varying_sequential': wandb.Image(fig5),
+                    'viz_varying_sequential_entropy': wandb.Image(fig6),
+                }, step=self.step_index)
                     
         if self.config['use_wandb'] and self.step_index > 0 and self.step_index > self.last_log_index + self.config['wandb_log_interval']:
             self.last_log_index = self.step_index
-            wandb.log({'log_loss': tf.math.log(loss), 'learning_rate': self.optimizer._decayed_lr(tf.float32)}, step=self.step_index)
+            wandb.log({'log_loss': tf.math.log(loss), 'learning_rate': self.evaler.optimizer._decayed_lr(tf.float32)}, step=self.step_index)
                     
         if self.step_index > 0 and self.step_index % self.config.test_interval == 0:
-            losses = self.test_loss(manager=manager)
+            losses = self.evaler.test_loss(manager=manager)
             
             wandb.log(losses, step=self.step_index)

cgt-mnist/vizualization.py
@@ -32,8 +32,6 @@ class Viz:
         batch = seq.shape[0]
         num_show_img = min(max_images, seq.shape[0])
         img = np.reshape(seq, (batch, *size, -1))
-        if img.shape[-1] == 1:
-            img = np.squeeze(img, axis=-1)    
 
         fig=plt.figure(figsize=(3*num_show_img, 3))
         for i in range(num_show_img):
@@ -44,22 +42,20 @@ class Viz:
         if return_fig:
             return fig
     
-    def unquantize(self, seq):
-        seq = tf.map_fn(fn=self.ds.unquantize, elems=seq, fn_output_signature=tf.float32)
-        return tf.squeeze(seq, axis=-1)
-    
     def scatter_on_bg(self, seq, idxs, output_length):
         batch_size = idxs.shape[0]
         seq_length = idxs.shape[1]
+        n_color_dims = 3
                       
         color = tf.constant(self.config.bg_color or [1., 0., 1.])
         bg = tf.tile(color[None, None, :], [batch_size, output_length, 1])
-
-        # convert seq to grayscale rgb
-        seq = tf.tile(seq[:, :, None], [1, 1, 3])
-
-        batch_idxs = tf.tile(tf.expand_dims(tf.range(batch_size), -1), [1, seq_length])
-        idxs_nd = tf.concat([tf.expand_dims(batch_idxs, -1), tf.expand_dims(idxs, -1)], axis=-1)
+        
+        batch_idxs = tf.range(batch_size)
+        color_idxs = tf.range(n_color_dims)
+        batch_idxs = tf.tile(batch_idxs[:, None, None], [1, seq_length, n_color_dims])
+        color_idxs = tf.tile(color_idxs[None, None, :], [batch_size, seq_length, 1])
+        idxs = tf.tile(idxs[:, :, None], [1, 1, n_color_dims])
+        idxs_nd = tf.stack([batch_idxs, idxs, color_idxs], axis=-1)
 
         seq = tf.tensor_scatter_nd_update(bg, idxs_nd, seq)
         
@@ -76,37 +72,17 @@ class Viz:
         seq_length = idxs.shape[1]
         height, width = size
         img_length = height*width
+        
+        # unquantize to rgb
         if do_unquantize:
-            seq = self.unquantize(seq)
-        else:
-            seq = tf.cast(seq, tf.float32)
+            seq = self.ds.unquantize(seq, to="rgb")
+        elif seq.shape[-1] == 1:
+            seq = self.ds.to_grayscale_rgb(seq)
+        
         if unshuffle:
             seq = self.scatter_on_bg(seq, idxs, img_length)
 
         return self.np_showSeq(seq, size, max_images, cmap, return_fig=return_fig)
-
-    def showSeqExpectedVal(self, seq, probs, idxs, size, max_images=3, cmap='gray', unshuffle=False, return_fig=False):
-        """ Show one or more images encoded as sequence. (tensorflow version)
-
-            seq: tensor of sequences which encode the image. Either a single sequence or multiple sequences.
-            size: the image size. e.g. (28, 28) for `mnist` images.
-            max_images: the maximum number of images to display.
-        """
-        batch_size = idxs.shape[0]
-        seq_length = idxs.shape[1]
-        height, width = size
-        img_length = height*width
-        
-        # do_unquantize for samples first
-        seq = tf.map_fn(fn=self.ds.unquantize, elems=seq, fn_output_signature=tf.float32)
-        
-        centroids = tf.reshape(self.centroids, [1, 1, -1])
-        expected_col = tf.tensordot(probs, centroids, axes=([2], [2]))
-        expected_col = tf.squeeze(expected_col, axis=-1)
-        
-        seq = tf.concat([seq, expected_col], axis=1)
-        seq = tf.squeeze(seq)
-        return self.showSeq(seq, idxs, size, max_images, cmap, unshuffle=unshuffle, do_unquantize=False, return_fig=return_fig)
 
     def compare_quantized_and_unquantized(self, dataset_test_original):
         